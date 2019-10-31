f8d689a00398a44635d61db6ef41a2133762e0cf
victoraugrosa <victoraugustorosa@gmail.com>
2019-10-31 18:31:16 -0300

    modificado teste

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,12 @@
 
 @section('content')
 <div class="row" style="text-align: center; margin-top:10%; margin-left:10px; font-size:18px;">
-  <div class="col-1 bg-white" style="border-color: black; border-style:solid; border-radius:20px; border-color:gray;">
-    <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-      <a class="nav-link btn btn-outline-dark" id="v-pills-home-tab" data-toggle="button" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-      <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-      <a class="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-      <a class="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
+  <div class="col-1 bg-white" style="border-width:1px; border-style:solid; border-radius:20px; border-color:black;">
+    <div class="nav flex-column nav-pills" style="  margin-top: 10px; margin-bottom: 10px;"id="v-pills-tab" role="tablist" aria-orientation="vertical">
+      <a class="nav-link btn btn-outline-danger" style="color: black; margin-bottom:5px;" id="v-pills-home-tab" data-toggle="button" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
+      <a class="nav-link btn btn-outline-danger" style="color: black; margin-bottom:5px;" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Guardiões</a>
+      <a class="nav-link btn btn-outline-danger" style="color: black; margin-bottom:5px;" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Conta</a>
+      <a class="nav-link btn btn-outline-danger" style="color: black; margin-bottom:5px;" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Sair</a>
     </div>
     
   </div>
